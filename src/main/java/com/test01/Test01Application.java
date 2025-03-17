91d592fb2af16200b5e5d3ae773a3b9233306d3f
dhana <dhana30@gmail.com>
2025-03-17 18:35:53 +0530

    defectfix-3456-login-error

--- src/main/java/com/test01/Test01Application.java
+++ src/main/java/com/test01/Test01Application.java
@@ -6,7 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Test01Application {
 
+
+
 	public static void main(String[] args) {
+
+int x= 1000;
+System.out.print(x);
 		SpringApplication.run(Test01Application.class, args);
 	}
 
