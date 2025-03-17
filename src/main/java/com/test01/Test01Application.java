8e50ed2d6082b7d9979394cac9d274ca8ce0e893
dhanas-s <dhananandhan30@gmail.com>
2025-03-17 18:51:08 +0530

    Update Test01Application.java
    bug rectified

--- src/main/java/com/test01/Test01Application.java
+++ src/main/java/com/test01/Test01Application.java
@@ -10,8 +10,9 @@ public class Test01Application {
 
 	public static void main(String[] args) {
 
-int x= 1000;
-System.out.print(x);
+        int x= 1000;
+        System.out.print(x);
+		int y =100;
 		SpringApplication.run(Test01Application.class, args);
 	}
 
